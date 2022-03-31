47494960883bb69af039a444a93c0ce0361bc84f
wjdanfu <wjdanfu@gmail.com>
2022-03-31 18:59:44 +0900

    test2 deploy

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
+BUILD_JAR=$(ls /home/ubuntu/build/build/libs/*.jar)
+JAR_NAME=$(basename $BUILD_JAR)
+echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/build/deploy.log
 
-REPOSITORY=/home/ubuntu/build
+echo "> build 파일 복사" >> /home/ubuntu/build/deploy.log
+DEPLOY_PATH=/home/ubuntu/sictak/
+cp $BUILD_JAR $DEPLOY_PATH
 
-APP_NAME=plogJPA
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
-JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
-
-CURRENT_PID=$(pgrep -f $APP_NAME)
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/build/deploy.log
+CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> Nothing to end."
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/build/deploy.log
 else
-  echo "> kill -9 $CURRENT_PID"
+  echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
-echo "> $JAR_PATH deploy"
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/build/deploy.log
+chmod +x $DEPLOY_JAR
+nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/build/deploy_err.log &
